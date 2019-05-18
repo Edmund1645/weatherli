60689efd8fcd05244e7074a644b0b1da3ac15f82
Edmund <edmund.timfon@gmail.com>
2019-05-18 23:37:38 +0100

    set up netlify-lambda locally

--- functions/weatherli.js
+++ functions/weatherli.js
@@ -1,6 +1,10 @@
+//import fetch from 'node-fetch';
+// require('dotenv').config();
+
 exports.handler = function(event, context, callback) {
+  console.log(process.env.WEATHERLI_KEY);
   callback(null, {
     statusCode: 200,
-    body: 'hello world'
+    body: 'Hello world'
   });
 };
